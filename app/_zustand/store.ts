2068d93afc9db1e13d4390dc009efed9c07295b1
h-rathi <thehimanshurathi@gmail.com>
2026-02-11 18:14:35 +0530

    change cart storage from session storage to local storage

--- app/_zustand/store.ts
+++ app/_zustand/store.ts
@@ -101,7 +101,7 @@ export const useProductStore = create<State & Actions>()(
     }),
     {
       name: "products-storage", // name of the item in the storage (must be unique)
-      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
     }
   )
 );
